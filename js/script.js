f4b3eff9ab6a10ae10a5e87ececf967c82f2008c
Krupa Patel <krupatel@live.unc.edu>
2020-11-18 19:07:20 -0500

    slideshow

---
+++ js/script.js
@@ -0,0 +1,81 @@
+function fadeOut() {
+  document.querySelector("div.circle").classList.add("fade-out");
+}
+
+
+
+
+
+// Gets all of the elements with the class "stats"
+// You'll have to add this to the svg elements manually
+var stats = document.querySelectorAll(".stat");
+
+// A little function that logs out whichever stat you clicked
+function logStat() {
+  console.log(this);
+}
+
+// A loop that adds a "click" event listener to every stat
+stats.forEach((stat) => {
+  stat.addEventListener("click", logStat);
+});
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("circle");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      // slides[i].style.display = "none"; 
+      slides[i].classList.add("fade-out"); 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
